26260048c0530c2a3feeb0d7036253ab9a22886f
Andreia <andreia.ca22@gmail.com>
2021-01-13 09:11:32 +0000

    functionality

--- src/App.js
+++ src/App.js
@@ -40,17 +40,19 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-      <FormControl>
-        <InputLabel>Write a Todo</InputLabel>
-        <Input  value={input} onChange={event => setInput(event.target.value)}/>
-      </FormControl>
-      <Button disabled={!input} type="submit" onClick={addTodo} variant="contained" color="primary">
-        Add Todo
-      </Button>
+    <div className="app">
+      <h1>To Do  ...</h1>
+      <div className="app__header">
+        <FormControl>
+          <InputLabel color="secondary">Write a Todo</InputLabel>
+          <Input  value={input} onChange={event => setInput(event.target.value)}/>
+        </FormControl>
+        <Button disabled={!input} type="submit" onClick={addTodo} variant="contained" color="primary">
+          Add Todo
+        </Button>
+      </div>
 
-      <ul>
+      <ul className="app__todoList">
         {todos.map(todo => (
           <Todo todo={todo}/>)
         )}
--- src/Todo.js
+++ src/Todo.js
@@ -2,46 +2,69 @@ import React, { useState } from 'react';
 import db from './firebase';
 
 import { makeStyles } from '@material-ui/core/styles';
-import { List, ListItem , ListItemText, ListItemAvatar, Modal} from '@material-ui/core';
+import EditIcon from '@material-ui/icons/Edit';
+import { List, ListItem , ListItemText, ListItemAvatar, Modal, Button, IconButton} from '@material-ui/core';
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 
 import './Todo.css';
 
 const useStyles = makeStyles((theme) => ({
+    modal: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
     paper: {
         position: 'absolute',
         width: 400,
         backgroundColor: theme.palette.background.paper,
-        border: '2px solid #000'
+        border: '2px solid #000',
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2,4,3)
     }
 }));
 
 function Todo(props) {
     const classes = useStyles();
     const [open, setOpen] = useState(false);
+    const [input, setInput] = useState('');
 
+    const updateTodo = () => {
+        // update toto with the new input text
+
+        db.collection('todos').doc(props.todo.id).set({
+            todo:input
+        }, { merge: true })
+        setOpen( false );
+    }
 
     return (
         <>
         <Modal 
+            className={classes.modal}
             open={open}
             onClose={e => setOpen(false)}
         >
-            <div>
-                <h1>I am a modal</h1>
-                <button onCLick={e => setOpen(false)}></button>
+            <div className={classes.paper}>
+                <h2>Edit me</h2>
+                <input placeholder={props.todo.todo} value={input} onChange={e => setInput(e.target.value)} />
+                <Button onClick={updateTodo}>Update Todo</Button>
             </div>
         </Modal>
-        <List>
+
+        <List className="list">
             <ListItem>
                 <ListItemAvatar>
                     <img src=""  alt=""/>
                 </ListItemAvatar>
                 <ListItemText primary={props.todo.todo} secondary="deadline"/>
             </ListItem>
-            <button onClick={e => setOpen(true)}></button>
+            <IconButton onClick={e => setOpen(true)}>
+                <EditIcon />
+            </IconButton>
             <DeleteForeverIcon onClick={e => db.collection('todos').doc(props.todo.id).delete()}/>
         </List>
+
         </>
     )
 }
